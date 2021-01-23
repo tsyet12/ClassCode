21a8ad9b1559eab051b488621df70646b0416e20
tsyet12 <tsyet12@gmail.com>
2021-01-23 01:01:51 +0100

    P10

---
+++ ClassCode/P10/Pd_query.py
@@ -0,0 +1,17 @@
+import pandas as pd
+
+data = {'A':  [1,2,3],
+        'B': [4,5,6],
+        'C': [7,8,9]}
+        
+
+df = pd.DataFrame(data, columns = ['A','B', 'C'], index=['First', 'Second', 'Third'])
+
+print(df.query("(A+B)%3==0"))
+print(df.query("(A+B)%3!!=0"))
+print(df.query("A*B*C<=80"))
+
+
+
+
+
---
+++ ClassCode/P10/Pd_where.py
@@ -0,0 +1,16 @@
+import pandas as pd
+
+data = {'A':  [1,2,3],
+        'B': [4,5,6],
+        'C': [7,8,9]}
+        
+
+df = pd.DataFrame(data, columns = ['A','B', 'C'], index=['First', 'Second', 'Third'])
+
+print(df.where(df<5,-df))
+
+print(df.where(df.isin([1,3,5]),-df))
+
+
+
+
